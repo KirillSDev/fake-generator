f52f7340c6e50853e0f7018d9d1b44f4a8d01b71
Kirill Starodubsev <colereteps@gmail.com>
2023-06-04 19:53:24 +0400

    feat: Add utility generateError

---
+++ src/utils/generateError.ts
@@ -0,0 +1,42 @@
+import random from 'random';
+import { generateType } from './generateType';
+import { Faker } from '@faker-js/faker';
+import { getCountryAlphabet } from './getCountryAlphabet';
+export const generateError = (faker: Faker, value: string, errors: number, locale: string) => {
+    const integerPart = Math.floor(errors);
+    const alphabet = getCountryAlphabet(locale);
+    let numberOfErrors = 0;
+    if (!Number.isInteger(errors) && random.float() <= errors % 1) {
+        numberOfErrors = 1;
+    }
+    const totalErrors = integerPart + numberOfErrors;
+    for (let i = 0; i < totalErrors; i++) {
+        const type = generateType();
+        const randomIndex = random.int(0, value.length);
+        const randomIndexByLocale = random.int(0, alphabet.length);
+        const permutationIndex = random.int(0, value.length - 1);
+        const letter = alphabet[randomIndexByLocale];
+        if (value.length < 40) {
+            switch (type) {
+                case 'delete':
+                    value = value.slice(0, randomIndex) + value.slice(randomIndex + 1);
+                    break;
+                case 'add':
+                    value = value.slice(0, randomIndex) + letter + value.slice(randomIndex);
+                    break;
+                case 'permute':
+                    let b = '';
+                    let a = value[permutationIndex];
+                    if (value.length === permutationIndex + 1) {
+                        b = value[permutationIndex - 1];
+                    } else {
+                        b = value[permutationIndex + 1];
+                    }
+                    value = value.slice(0, permutationIndex) + b + a + value.slice(permutationIndex + 2);
+                    break;
+            }
+        }
+    }
+
+    return value;
+};
